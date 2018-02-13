62d159daa15d062eaa87b877b7c3bf5dac15aac8
Barbe-Bleue <screy@icloud.com>
2018-02-13 12:35:49 +0100

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) =>
+  res.send("salut")
+)
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /users page.
+app.get('/users', function (req, res) {
+   console.log("Got a GET request for /users");
+   res.send('Page Listing');
+})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
