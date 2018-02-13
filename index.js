3db7b461b065d84dea8a0f42f300d8fc609ef8a4
Barbe-Bleue <screy@icloud.com>
2018-02-13 16:57:45 +0100

    add mongo connection

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const fetch = require ('node-fetch');
 const cheerio = require ('cheerio');
-const request = require ('request')
-
+const request = require ('request');
+mongoose.connect("mongodb://localhost/test");
 app.get('/', (req, res) =>
   res.send("Bienvenue sur notre Pokenode")
 )
 
-// This responds a POST request for the homepage
 app.get('/pokemons', function (req, res) {
    console.log("Got a POST request for the homepage");
    let pokemon;
@@ -19,25 +19,20 @@ app.get('/pokemons', function (req, res) {
      }console.log(pokemon);
      res.send('Liste de tous les pokémons \n'+pokemon);
    });
-
 })
 
-// This responds a POST request for the homepage
 app.get('/pokemons/:id', function (req, res) {
    console.log("Got a POST request for the homepage");
    res.send('Affiche le pokémon n°' + req.params.id);
 })
 
-// This responds a DELETE request for the /del_user page.
 app.delete('/pokemon/:id', function (req, res) {
    console.log("Got a DELETE request for /del_user");
    res.send('Hello DELETE');
 })
 
-
 // PARTIE USER
 
-// This responds a GET request for the /users page.
 app.get('/users', function (req, res) {
    console.log("Got a GET request for /users");
    res.send('Liste des utilisateurs');
@@ -47,6 +42,5 @@ app.get('/users/:id', function (req, res) {
    console.log("Got a POST request for the homepage");
    res.send('Affiche l utilisateur n°' + req.params.id);
 })
-
 
 app.listen(3000, () => console.log('App is live on port 3000!'))
