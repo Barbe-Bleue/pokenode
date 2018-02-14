deaae99954c58d169e9c70821378310e1cb5904c
Barbe-Bleue <screy@icloud.com>
2018-02-14 12:31:39 +0100

    add get evolution function

--- index.js
+++ index.js
@@ -16,37 +16,64 @@ app.get('/', (req, res) =>
 app.get('/pokemons', function (req, res) {
   console.log("Got a POST request for the homepage");
   let pokedex = [];
+  let finalPokedex = [];
   fetch("http://www.pokemontrash.com/pokedex/liste-pokemon.php")
-    .then(res => res.text())
-    .then(html => cheerio.load(html))
-    .then($ => {
-      $("#pokedex-list table.pokedex:nth-child(14) tbody").each((index,element) => {
-        $(element).children("tr").each((index,element2) =>{
-          let type2 = "";
-          let pokemon = $(element2).children("td");
-          let id = $(element2).children("td:first-child").text();
-          let name = pokemon.children("strong").children("a.name").text();
-          let thumbails = "http://www.pokemontrash.com/pokedex/"+pokemon.children("img").attr("src");
-          let idImage = addZeroToId(id);
-          let image = "http://www.pokemontrash.com/pokedex/images/sugimori/"+idImage+".png";
-          let type = pokemon.children("span.type1").text();
+  .then(res => res.text())
+  .then(html => cheerio.load(html))
+  .then($ => {
+    $("#pokedex-list table.pokedex:last-child tbody tr").each((index,element) => {
+      let type2 = "";
+      let pokemon = $(element).children("td");
+      let id = $(element).children("td:first-child").text();
+      let name = pokemon.children("strong").children("a.name").text();
+      let thumbails = "http://www.pokemontrash.com/pokedex/"+pokemon.children("img").attr("src");
+      let idImage = addZeroToId(id);
+      let image = "http://www.pokemontrash.com/pokedex/images/sugimori/"+idImage+".png";
+      let type = pokemon.children("span.type1").text();
 
-          if(typeof(pokemon.children("span.type2").text()) != "undefined"){
-            type2 = pokemon.children("span.type2").text();
-          }
+      if(typeof(pokemon.children("span.type2").text()) != "undefined"){
+        type2 = pokemon.children("span.type2").text();
+      }
 
-          pokedex.push({
-            "id":id,
-            "name":name,
-            "thumbails": thumbails,
-            "image":image,
-            "type": type,
-            "type2": type2
-          })
-        });
-        res.send(pokedex);
-      });
+      pokedex.push({
+        "id":id,
+        "name":name,
+        "thumbails": thumbails,
+        "image":image,
+        "type": type,
+        "type2": type2
+      })
     });
+    for(let pokemon of pokedex){
+      let evolutions = [];
+      fetch("http://www.pokemontrash.com/pokedex/"+pokemon.id+"-"+pokemon.name+".html")
+      .then(res => res.text())
+      .then(html => cheerio.load(html))
+      .then($ => {
+        $("div#Stages div").each((index,element) => {
+          evolutionName = $(element).children("a").text();
+          if($(element).children("span.evolution-trigger").text().match(/\d/g) != null){
+            evolutionLvl = $(element).children("span.evolution-trigger")
+            .text()
+            .match(/\d/g)
+            .join("");
+          }else {
+            evolutionLvl = "1";
+          }
+          evolutions.push({
+            "evolutionName": evolutionName,
+            "niveauEvoltution": evolutionLvl
+          })
+          const newPokedex = {
+            ...pokemon,
+            evolutions
+          };
+          finalPokedex.push(newPokedex);
+        });
+      });
+    }
+  });
+  res.send(finalPokedex);
 })
 
 app.get('/pokemons/:id', function (req, res) {
