acfb95e03415a4be3101fe64146f6ca0071c3f61
BERANGER Aurélien <aurelien.beranger@gmail.com>
2018-02-15 15:34:01 +0100

    Add files via upload
    Vue HTML/CSS tu peux renommer le php en html

---
+++ view/index.php
@@ -0,0 +1,323 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>PokeNode</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="images/favicon.ico">
+
+        <!-- Google Font link -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!--For Plugins external css-->
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="styles.css">
+
+
+    </head>
+
+    <body >
+        
+        <section  class="jumbotron text-center">
+            <div class="container">
+
+         <div class="container" style="background: rgba(80, 79, 79, 0.44);border-radius: 30px;color: white;">
+        <p><h2>Qu'est-ce que PokéNode ?</h2>
+        Déjà <b>6.235.099</b> dresseurs<br>
+        PokéNode est un Pokédex interactif qui montre toutes les informations des Pokémon<br>
+        Vous pouvez également retrouver les meilleurs attaques de vos Pokemon et l'ensemble de leurs statistiques grâce à notre Pokedex. Soutenez-nous en nous envoyant vos dons via Paypal pour que nos services s'améliorent.</p>
+        <form name="_xclick" action="https://www.paypal.com/ne/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_xclick">
+            <input type="hidden" name="business" value="pseudonyme@monentreprise.com">
+            <input type="hidden" name="item_name" value="Restos du Coeur">
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="hidden" name="amount" value="25.00">
+            <input type="image" src="http://www.paypal.com/fr_XC/i/btn/x-click-butcc-donate.gif" border="0" name="submit" alt="Effectuez vos paiements via PayPal : une solution rapide, gratuite et sécurisée">
+            </form>
+        </div>
+                <!-- <h1>Pokedex Project</h1> 
+                <p>Liste des pokémon</p> -->
+                <!--<div class="btn btn-info btn-large">Download </div>-->
+            </div>
+        </section>
+
+        <div class="text-center" style="display: flex;justify-content: center;">
+            <img src="images/pokedex.png" style="float:left;height:100px;">
+            <h1 class="titre-pokedex">Pokedex <img src="images/icon4.png" style="height:40px;margin-bottom:9px;"></h1> 
+        </div>
+
+
+        <section id="bs-pricing" class="bs-pricing roomy-50 bg-white fix">
+            <div class="container">
+
+                <!-- Début de la ligne de 3 pokémon -->
+                <div class="col-md-12 ligne-pkm">
+                    <div class="row">
+                        <div class="bs-pricing-table-two text-center">
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/bulbizarre.png" alt="" />
+                                    </div>
+                                    <div class="head ">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #001</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Bulbizarre</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/poison.gif"> <img src="images/plante.gif"></li>
+                                    </ul>
+                                    
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/herbizarre.png" alt="" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #002</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Herbizarre</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/poison.gif"> <img src="images/plante.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-sm-offset-3 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/florizarre.png" alt="" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #003</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Florizarre</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/poison.gif"> <img src="images/plante.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin de la ligne de 3 pokémon -->
+
+                <!-- Début de la ligne de 3 pokémon -->
+                <div class="col-md-12 ligne-pkm">
+                    <div class="row">
+                        <div class="bs-pricing-table-two text-center">
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/salameche.png" alt="" />
+                                    </div>
+                                    <div class="head ">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #004</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Salamèche</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/feu.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-m-top-140 xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/reptincel.png" alt="" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #005</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Reptincel</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/feu.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-sm-offset-3 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item last-border sm-m-top-140">
+                                    <div class="bs-icon">
+                                        <img src="images/dracaufeu.png" alt="" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #006</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Dracaufeu</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/vol.gif"> <img src="images/feu.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin de la ligne de 3 pokémon -->
+
+ <!-- Début de la ligne de 3 pokémon -->
+                <div class="col-md-12 ligne-pkm">
+                    <div class="row">
+                        <div class="bs-pricing-table-two text-center">
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/carapuce.png" alt="carapuce" />
+                                    </div>
+                                    <div class="head ">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #007</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Carapuce</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/eau.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item xs-m-top-140 xs-last-border">
+                                    <div class="bs-icon">
+                                        <img src="images/carabaffe.png" alt="carabaffe" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #008</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Carabaffe</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/eau.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                            <!-- DEBUT FICHE POKEMON -->
+                            <div class="col-md-4 col-sm-6 col-sm-offset-3 col-xs-12 fiche-pkm">
+                                <div class="bs-pricing-item last-border sm-m-top-140">
+                                    <div class="bs-icon">
+                                        <img src="images/tortank.png" alt="tortank" />
+                                    </div>
+                                    <div class="head">
+                                        <div class="price-title">
+                                            <h2 class="text-uppercase">ID #009</h2>
+                                            <div class="separator"></div>
+                                        </div>
+                                        <div class="price">
+                                            <h1>Tortank</h1>
+                                        </div>
+                                    </div>
+
+                                    <ul class="text-uppercase">
+                                        <li><img src="images/eau.gif"></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- FIN FICHE POKEMON -->
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin de la ligne de 3 pokémon -->
+            </div>
+            <!-- Fin de la balise container -->
+        </section>
+
+       
+
+
+
+        <footer id="footer" class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="copyright text-center">
+                        <p class="wow fadeInRight">LENFANT Valentin - BERANGER Aurélien - JEAN Guillaume <br>2018. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+        <!-- JS includes -->
+
+
+    </body>
+</html>
