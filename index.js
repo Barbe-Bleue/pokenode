33acf952cb8e054180cbdb0e9ee834bfd504c750
Barbe-Bleue <screy@icloud.com>
2018-02-15 09:35:36 +0100

    update require & update route

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const app = express();
 const fetch = require ('node-fetch');
 const cheerio = require ('cheerio');
 const request = require ('request');
-const addZeroToId = require('./functions.js');
-const getEvolution = require('./evolution.js');
-const getPokemon = require('./getAllpokemon.js')
 
 mongoose.connect("mongodb://localhost/test");
 
@@ -15,21 +12,11 @@ app.get('/', (req, res) =>
 )
 
 app.get('/pokemons', function (req, res) {
-  res.send(getPokemon());
+  //res.send(getPokemon());
 });
 
 app.get('/pokemons/:id', function (req, res) {
-  console.log("Got a POST request for the homepage");
-  let pokedex = [];
-  request("https://pokeapi.co/api/v2/pokemon/"+req.params.id, function(err, response, json){
-    let pokemon = JSON.parse(json);
-    pokedex.push({
-      "id": pokemon.id,
-      "name": pokemon.name,
-      "type": pokemon.types[0].type.name
-    });
-    res.send(pokedex);
-  });
+  
 })
 
 app.delete('/pokemon/:id', function (req, res) {
