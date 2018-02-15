372f26fd3c334575fdb5a982b18afdef4b0d4cdc
Barbe-Bleue <screy@icloud.com>
2018-02-15 14:37:14 +0100

    some utility functions

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function addZeroToId(id){
+module.exports.addZeroToId = function (id){
   if(id >= 0 && id <= 9){
     id = "00"+id;
   }else if (id >=10 && id <= 99 ) {
@@ -8,4 +8,12 @@ function addZeroToId(id){
   }
   return id;
 }
-module.exports = addZeroToId;
+
+module.exports.getObjectKeyIndex = function (obj, keyToFind) {
+  var key;
+  for (key in obj) {
+    if (key == keyToFind)
+      return true
+  }
+  return null;
+}
