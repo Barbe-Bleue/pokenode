cd09bd952948304f61ffce5ebcfe2a92005b8d82
Barbe-Bleue <screy@icloud.com>
2018-02-16 12:24:40 +0100

    add find and check login user

--- query/userquery.js
+++ query/userquery.js
@@ -28,6 +28,33 @@ module.exports.isUserExist = async function(email) {
   return isExist;
 }
 
+module.exports.checkLogin = async function (infoUser,res) {
+  const user = await this.findUserByeEmail(infoUser.email,res);
+  if(user)
+    //let accepted = bcrypt.compareSync(""+infoUser.password, user.hash_password);
+    if(!accepted)
+      res.status(400).json("Email ou mot de passe incorrect");
+    else
+      return res.json({
+        token: jwt.sign({
+          email: user.email,
+          fullName: user.name,
+          _id: user._id,
+          expiresInMinutes: 5 // expires in 5 min
+        },"RESTFULAPIs")
+      });
+  else
+    res.status(400).json("Email ou mot de passe incorrect");
+}
+module.exports.findUserByEmail = async function (email,res) {
+  const oneUser = await Client.findOne()
+    .where("email")
+    .eq(email)
+  if(oneUser)
+    res.json(oneUser);
+  else
+    res.status(404).json("Le user n'existe pas !");
+}
 module.exports.findUserById = async function(id,res) {
   const oneUser = await Client.findOne()
     .where("_id")
@@ -54,7 +81,7 @@ module.exports.addUser = async function(infoUser,res){
     // Check si les champs correspondent au Schema
     let problemo = checkInfoUserWithSchema(infoUser);
     if(!problemo){
-      infoUser.password = bcrypt.hashSync(infoUser.password, 10)
+      infoUser.password = bcrypt.hashSync(infoUser.password)
       new Client(infoUser).save();
       res.json("Le user "+infoUser.name+" a été ajouté !");
     }else
@@ -63,6 +90,12 @@ module.exports.addUser = async function(infoUser,res){
     res.status(200).json("Le user existe déjà !");
 }
 
+module.exports.login = async function functionName(infoUser,res) {
+  const isCorrect = await this.checkUser(infoUser);
+  if(isCorrect){
+
+  }
+}
 function checkInfoUserWithSchema(infoUser){
   for(let key in infoUser ){
     // check si les champs correspondent au Schema
